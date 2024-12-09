d18a2b334c8600b5eff0b261fa91f243a6c5f56c
Trekels <vincent.trekels@gmail.com>
2024-12-09 10:05:57 +0100

    Small config tweaks

--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+error_reporting(E_ERROR | E_PARSE); // Disable warnings and notices
+set_time_limit(0); // To default time limit
+
 use League\Container as LeagueContainer;
 use Slim\Factory\AppFactory;
 
